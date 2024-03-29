445e08019b4269ce9782713ffa45ea2f36e1ded0
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-29 15:56:02 -0500

    Startes something

--- assets/script/app.js
+++ assets/script/app.js
@@ -16,5 +16,32 @@ function select(selector) {
     return document.querySelector(selector);
 }
 
+const input = select('.input');
+const searchButton = select('.create');
 const searchDialog = select('.search-dialog');
+const searchDialogList = select('.search-dialog-list');
 const selectedMovie = select('.selected-movie');
+
+const matchesFound = [];
+function search() {
+    if (input.value.length > 2) {
+        searchDialog.classList.add('visible');
+        for (let i = 0; i < movies.length; i++) {
+            const currentMovie = movies[i].title;
+
+            if (input.value.split('').some(char => currentMovie.includes(char))) {
+                searchDialogList.innerHTML += `<li>${currentMovie}</li>`;
+            }
+        }
+    } else {
+        searchDialog.classList.remove('visible');
+    }
+
+    setTimeout(() => {
+        search();
+    }, 1000);
+}
+
+listen('input', input, function() {
+    search();
+});
