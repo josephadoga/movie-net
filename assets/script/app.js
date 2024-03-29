e074a05c728242f5f17ab3a2e22926da489d9943
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-29 18:58:27 -0500

    Fixed Selection

--- assets/script/app.js
+++ assets/script/app.js
@@ -17,31 +17,53 @@ function select(selector) {
 }
 
 const input = select('.input');
-const searchButton = select('.create');
+const searchButton = select('.search-movie');
 const searchDialog = select('.search-dialog');
 const searchDialogList = select('.search-dialog-list');
+
 const selectedMovie = select('.selected-movie');
 
 const matchesFound = [];
+
+
 function search() {
-    if (input.value.length > 2) {
-        searchDialog.classList.add('visible');
+    searchDialogList.innerHTML = '';
+    matchesFound.length = 0;
+    let inputSearch = input.value.trim().toLowerCase();
+    if (inputSearch.length > 2) {
         for (let i = 0; i < movies.length; i++) {
             const currentMovie = movies[i].title;
 
-            if (input.value.split('').some(char => currentMovie.includes(char))) {
-                searchDialogList.innerHTML += `<li>${currentMovie}</li>`;
+            if (currentMovie.trim().toLowerCase().includes(inputSearch)) {
+                searchDialog.classList.add('visible');
+                searchDialogList.innerHTML += `<li class="movie">${currentMovie}</li>`;
+                matchesFound.push(currentMovie);
             }
+        }
+        if (matchesFound.length === 0) {
+            searchDialog.classList.add('visible');
+            searchDialogList.innerHTML += `<li class="notfound">Movie not found</li>`;
         }
     } else {
         searchDialog.classList.remove('visible');
     }
-
-    setTimeout(() => {
-        search();
-    }, 1000);
 }
+
+function whenClicked(event) {
+    const clickedMovieTitle = event.target.textContent;
+    input.value = clickedMovieTitle;
+    searchDialog.classList.remove('visible');
+}
+
+function selection() {
+    const movieListItems = document.querySelectorAll('.movie');
+    movieListItems.forEach(item => {
+        item.addEventListener('click', whenClicked);
+    });
+}
+
 
 listen('input', input, function() {
     search();
+    selection();
 });
